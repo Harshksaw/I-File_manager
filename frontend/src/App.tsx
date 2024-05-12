811f9a71ff63bada131247a52868a397a3d2c367
Harsh kumar <mister.harshkumar@gmail.com>
2024-05-12 14:04:43 +0530

    Frontend AUTH

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,34 @@
-import { useState } from 'react';
-import axios from 'axios';
-import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+
+
+import Home from "./pages/Home";
+
+
+import HeaderSection from "./components/Header";
+import Dashboard from "./pages/DashBoard";
+
 
 function App() {
-  
-  
-
   return (
-    <div className=''>
+    <>
+       <BrowserRouter>
+        <HeaderSection/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
 
-    
-  
-    </div>
-  );
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
